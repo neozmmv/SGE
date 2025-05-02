d84273b52f82e7734b58c137074c9e9b0cb64b1f
Enzo Gutiérrez Pereira <83484706+neozmmv@users.noreply.github.com>
2025-05-02 17:23:42 -0300

    Delete db.py

--- db.py
+++
@@ -1,9 +0,0 @@
-import mysql.connector
-
-def get_connection():
-    return mysql.connector.connect(
-        host="IP_SERVER",
-        user="USER",
-        password="PASSWORD",
-        database="DATABASE"
-    )
