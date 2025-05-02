b507591fc2b1fa68c92e0a8d144bd33c0645e139
Enzo Gutiérrez Pereira <enzogp2005@gmail.com>
2025-05-02 17:18:43 -0300

    Update db.py

--- db.py
+++ db.py
@@ -2,8 +2,8 @@ import mysql.connector
 
 def get_connection():
     return mysql.connector.connect(
-        host="",          # ex: "meubanco.mysql.uhserver.com"
-        user="",       # ex: "admin"
-        password="",     # ex: "minhasenha123"
-        database="" # nome do seu banco criado
+        host="168.75.73.251",          # ex: "meubanco.mysql.uhserver.com"
+        user="admin_sge",       # ex: "admin"
+        password="AdminSGE09042@12",     # ex: "minhasenha123"
+        database="gestao_escolar" # nome do seu banco criado
     )
